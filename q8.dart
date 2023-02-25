9bae7a3ad687a569b4f29110cc5f00d773d2ef8e
aizazaltaf <aizazaltaf@gmail.com>
2023-02-25 17:26:26 +0500

    assignment 3

---
+++ q8.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  Map<String, String> directory = {'name': 'Matt', 'phone': '03332037964'};
+
+  var newKey = directory.keys.where((element) => element.length == 4);
+
+  print(newKey);
+}
